c547e2f2aead1bd28de4907bab4b2ae283a3e2dc
Imcasero <diegocaserosmr@gmail.com>
2024-12-13 12:53:18 +0100

    feat: add list module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
+import { ListModule } from './list/list.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [UserModule, ListModule],
 })
 export class AppModule {}
